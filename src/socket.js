ab8fece3e7f975a47c93c6b3191ebbf83bda7124
pioneeringdev <codedec116@gmail.com>
2019-08-15 05:30:52 -0400

    use api endpoint as env variable

--- src/socket.js
+++ src/socket.js
@@ -1,14 +1,14 @@
 
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3000');
+const socket = io(process.env.REACT_APP_API_ENDPOINT);
 
 const subscribeToTimer = (cb) => {
     socket.on('data', data => cb(data));
-}
+};
 
 const disconnect = () => {
     socket.disconnect();
-}
+};
 
 export { subscribeToTimer, disconnect };
