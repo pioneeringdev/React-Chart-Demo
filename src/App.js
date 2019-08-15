9699af9964504c9850769d18154ea5966c6a148a
pioneeringdev <codedec116@gmail.com>
2019-08-15 04:28:00 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Chart from './chart';
+function App() {
+  return (
+    <div className="App">
+      <Chart/>
+    </div>
+  );
+}
+
+export default App;
